42b7a239434545b2b054c9e72061eb4b000a2d50
brmcdoug <brmcdoug@cisco.com>
2023-05-17 20:32:00 -0700

    udp

--- sonic-vs/util/scapy/srv6-probe-udp.py
+++ sonic-vs/util/scapy/srv6-probe-udp.py
@@ -11,7 +11,7 @@ from scapy.all import *
 
 p0 = Ether(src = "fe:54:00:33:cd:75", dst = "52:54:00:74:c1:01") \
 / IPv6(src = "2001:0:101:1::4", dst = "fc00:0:3:7:11:1:6500::") \
-/ IP(src = "10.101.11.1", dst = "10.101.1.4") / UDP(dport=33333)/payload
+/ IP(src = "10.101.11.1", dst = "10.101.1.4") / UDP(sport=22222, dport=33333)
 p0.show()
 sendp(p0, iface="sonic01-16", count=64)
 
