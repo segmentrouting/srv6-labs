e693389142ebfc05f0d350a0bf1637e8a4518706
brmcdoug <brmcdoug@cisco.com>
2023-12-04 13:59:58 -0800

    clab update

--- 2-use-case-topologies/clab-xrd/20-node-add-bridges.sh
+++ 2-use-case-topologies/clab-xrd/20-node-add-bridges.sh
@@ -3,7 +3,9 @@
 sudo brctl addbr xrd06-host
 sudo brctl addbr xrd09-host
 sudo brctl addbr xrd46-host
+sudo brctl addbr frr96-host
 
 sudo ip link set up xrd06-host
 sudo ip link set up xrd09-host
 sudo ip link set up xrd46-host
+sudo ip link set up frr96-host
