fd900ecd8d2213d5ac566ca3c4e57a8efa3fba44
brmcdoug <brmcdoug@cisco.com>
2024-01-03 20:38:04 -0800

    trex

--- 2-use-case-topologies/peterson-graph/trex/imix-start.py
+++ 2-use-case-topologies/peterson-graph/trex/imix-start.py
@@ -1,64 +1,70 @@
+
+import sys
+
+# adding trex location to the system path
+sys.path.insert(0, '/home/cisco/trex-v3.04/trex_client/interactive/')
+
 from trex_stl_lib.api import *
 
 # trex01
 a = STLClient(server = "172.20.99.101")
 a.connect()
 a.reset()
-a.start_line (" -f 1-imix.py -m 3mbps --port 0")
+a.start_line (" -f trex01/1-imix.py -m 3mbps --port 0")
 
 # trex02
 b = STLClient(server = "172.20.99.102")
 b.connect()
 b.reset()
-b.start_line (" -f 2-imix.py -m 4mbps --port 0")
+b.start_line (" -f trex02/2-imix.py -m 4mbps --port 0")
 
 # trex03
 c = STLClient(server = "172.20.99.103")
 c.connect()
 c.reset()
-c.start_line (" -f 3-imix.py -m 5mbps --port 0")
+c.start_line (" -f trex03/3-imix.py -m 5mbps --port 0")
 
 # trex04
 d = STLClient(server = "172.20.99.104")
 d.connect()
 d.reset()
-d.start_line (" -f 4-imix.py -m 6mbps --port 0")
+d.start_line (" -f trex04/4-imix.py -m 6mbps --port 0")
 
 # trex05
 e = STLClient(server = "172.20.99.105")
 e.connect()
 e.reset()
-e.start_line (" -f 5-imix.py -m 2mbps --port 0")
+e.start_line (" -f trex05/5-imix.py -m 2mbps --port 0")
 
 # trex06
 f = STLClient(server = "172.20.99.106")
 f.connect()
 f.reset()
-f.start_line (" -f 6-imix.py -m 11mbps --port 0")
+f.start_line (" -f trex06/6-imix.py -m 11mbps --port 0")
 
 # trex07
 g = STLClient(server = "172.20.99.107")
 g.connect()
 g.reset()
-g.start_line (" -f 7-imix.py -m 4mbps --port 0")
+g.start_line (" -f trex07/7-imix.py -m 4mbps --port 0")
 
 # trex08
 h = STLClient(server = "172.20.99.108")
 h.connect()
 h.reset()
-h.start_line (" -f 8-imix.py -m 15mbps --port 0")
+h.start_line (" -f trex08/8-imix.py -m 15mbps --port 0")
 
 # trex09
 i = STLClient(server = "172.20.99.109")
 i.connect()
 i.reset()
-i.start_line (" -f 9-imix.py -m 10mbps --port 0")
+i.start_line (" -f trex09/9-imix.py -m 10mbps --port 0")
 
 # trex10
 j = STLClient(server = "172.20.99.110")
 j.connect()
 j.reset()
-j.start_line (" -f 10-imix.py -m 10mbps --port 0")
+j.start_line (" -f trex10/10-imix.py -m 10mbps --port 0")
 
 # a.stop(ports = [0])
 # b.stop(ports = [0])
--- 2-use-case-topologies/peterson-graph/trex/trex-setup.sh
+++ 2-use-case-topologies/peterson-graph/trex/trex-setup.sh
@@ -5,6 +5,7 @@
 ## ubuntu trex 01
 docker exec -it clab-trex-ubtrex01 sysctl -p
 docker exec -it clab-trex-ubtrex01 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.1.2/24 dev eth1
 docker exec -it clab-trex-ubtrex01 ip addr add fc00:0:101:1::101/64 dev eth1
 docker exec -it clab-trex-ubtrex01 ip route add 10.0.0.0/8 via 10.101.1.1 dev eth1
 docker exec -it clab-trex-ubtrex01 ip -6 route add fc00:0000::/32 via fc00:0:101:1::1 dev eth1
@@ -12,6 +13,7 @@ docker exec -it clab-trex-ubtrex01 ip -6 route add fc00:0000::/32 via fc00:0:101
 ## ubuntu trex 02
 docker exec -it clab-trex-ubtrex02 sysctl -p
 docker exec -it clab-trex-ubtrex02 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.2.2/24 dev eth1
 docker exec -it clab-trex-ubtrex02 ip addr add fc00:0:101:2::102/64 dev eth1
 docker exec -it clab-trex-ubtrex02 ip route add 10.0.0.0/8 via 10.101.2.1 dev eth1
 docker exec -it clab-trex-ubtrex02 ip -6 route add fc00:0000::/32 via fc00:0:101:2::1 dev eth1
@@ -19,63 +21,63 @@ docker exec -it clab-trex-ubtrex02 ip -6 route add fc00:0000::/32 via fc00:0:101
 ## ubuntu trex 03
 docker exec -it clab-trex-ubtrex03 sysctl -p
 docker exec -it clab-trex-ubtrex03 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.3.2/24 dev eth1
 docker exec -it clab-trex-ubtrex03 ip addr add fc00:0:101:3::103/64 dev eth1
-docker exec -it clab-trex-ubtrex03 ip addr add fc00:0:101:7::103/64 dev eth2
 docker exec -it clab-trex-ubtrex03 ip route add 10.0.0.0/8 via 10.101.3.1 dev eth1
 docker exec -it clab-trex-ubtrex03 ip -6 route add fc00:0000::/32 via fc00:0:101:3::1 dev eth1
 
 ## ubuntu trex 04
 docker exec -it clab-trex-ubtrex04 sysctl -p
 docker exec -it clab-trex-ubtrex04 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.4.2/24 dev eth1
 docker exec -it clab-trex-ubtrex04 ip addr add fc00:0:101:4::104/64 dev eth1
-docker exec -it clab-trex-ubtrex04 ip addr add fc00:0:101:8::104/64 dev eth2
 docker exec -it clab-trex-ubtrex04 ip route add 10.0.0.0/8 via 10.101.4.1 dev eth1
 docker exec -it clab-trex-ubtrex04 ip -6 route add fc00:0000::/32 via fc00:0:101:4::1 dev eth1
 
 ## ubuntu trex 05
 docker exec -it clab-trex-ubtrex05 sysctl -p
 docker exec -it clab-trex-ubtrex05 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.5.2/24 dev eth1
 docker exec -it clab-trex-ubtrex05 ip addr add fc00:0:101:5::105/64 dev eth1
-docker exec -it clab-trex-ubtrex05 ip addr add fc00:0:101:9::105/64 dev eth2
 docker exec -it clab-trex-ubtrex05 ip route add 10.0.0.0/8 via 10.101.5.1 dev eth1
 docker exec -it clab-trex-ubtrex05 ip -6 route add fc00:0000::/32 via fc00:0:101:5::1 dev eth1
 
 ## ubuntu trex 06
 docker exec -it clab-trex-ubtrex06 sysctl -p
 docker exec -it clab-trex-ubtrex06 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.6.2/24 dev eth1
 docker exec -it clab-trex-ubtrex06 ip addr add fc00:0:101:6::106/64 dev eth1
-docker exec -it clab-trex-ubtrex06 ip addr add fc00:0:101:10::106/64 dev eth2
 docker exec -it clab-trex-ubtrex06 ip route add 10.0.0.0/8 via 10.101.6.1 dev eth1
 docker exec -it clab-trex-ubtrex06 ip -6 route add fc00:0000::/32 via fc00:0:101:6::1 dev eth1
 
 ## ubuntu trex 07
 docker exec -it clab-trex-ubtrex07 sysctl -p
 docker exec -it clab-trex-ubtrex07 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.7.2/24 dev eth1
 docker exec -it clab-trex-ubtrex07 ip addr add fc00:0:101:7::107/64 dev eth1
-docker exec -it clab-trex-ubtrex07 ip addr add fc00:0:101:1::107/64 dev eth2
 docker exec -it clab-trex-ubtrex07 ip route add 10.0.0.0/8 via 10.101.7.1 dev eth1
 docker exec -it clab-trex-ubtrex07 ip -6 route add fc00:0000::/32 via fc00:0:101:7::1 dev eth1
 
 ## ubuntu trex 08
 docker exec -it clab-trex-ubtrex08 sysctl -p
 docker exec -it clab-trex-ubtrex08 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.8.2/24 dev eth1
 docker exec -it clab-trex-ubtrex08 ip addr add fc00:0:101:8::108/64 dev eth1
-docker exec -it clab-trex-ubtrex08 ip addr add fc00:0:101:2::108/64 dev eth2
 docker exec -it clab-trex-ubtrex08 ip route add 10.0.0.0/8 via 10.101.8.1 dev eth1
 docker exec -it clab-trex-ubtrex08 ip -6 route add fc00:0000::/32 via fc00:0:101:8::1 dev eth1
 
 ## ubuntu trex 09
 docker exec -it clab-trex-ubtrex09 sysctl -p
 docker exec -it clab-trex-ubtrex09 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.9.2/24 dev eth1
 docker exec -it clab-trex-ubtrex09 ip addr add fc00:0:101:9::109/64 dev eth1
-docker exec -it clab-trex-ubtrex09 ip addr add fc00:0:101:3::109/64 dev eth2
 docker exec -it clab-trex-ubtrex09 ip route add 10.0.0.0/8 via 10.101.9.1 dev eth1
 docker exec -it clab-trex-ubtrex09 ip -6 route add fc00:0000::/32 via fc00:0:101:9::1 dev eth1
 
 ## ubuntu trex 10
 docker exec -it clab-trex-ubtrex10 sysctl -p
 docker exec -it clab-trex-ubtrex10 systemctl start trex
+docker exec -it clab-trex-ubtrex01 ip addr add 10.101.10.2/24 dev eth1
 docker exec -it clab-trex-ubtrex10 ip addr add fc00:0:101:10::110/64 dev eth1
-docker exec -it clab-trex-ubtrex10 ip addr add fc00:0:101:4::110/64 dev eth2
 docker exec -it clab-trex-ubtrex10 ip route add 10.0.0.0/8 via 10.101.10.1 dev eth1
 docker exec -it clab-trex-ubtrex10 ip -6 route add fc00:0000::/32 via fc00:0:101:10::1 dev eth1
