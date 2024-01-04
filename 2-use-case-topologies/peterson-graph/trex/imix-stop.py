1c9edb4f60dcad340a6575dd9f00eb0a6ccf5358
brmcdoug <brmcdoug@cisco.com>
2024-01-03 22:15:39 -0800

    imix

--- 2-use-case-topologies/peterson-graph/trex/imix-stop.py
+++ 2-use-case-topologies/peterson-graph/trex/imix-stop.py
@@ -1,3 +1,8 @@
+import sys
+
+# adding trex location to the system path
+sys.path.insert(0, '/home/cisco/trex-v3.04/trex_client/interactive/')
+
 from trex_stl_lib.api import *
 
 a = STLClient(server = "172.20.99.101")
