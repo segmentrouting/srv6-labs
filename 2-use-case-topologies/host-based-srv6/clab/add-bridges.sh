e39f96925714bde71723aca995870d0b109e712c
brmcdoug <brmcdoug@cisco.com>
2024-07-31 12:50:23 -0700

    host

--- 2-use-case-topologies/host-based-srv6/clab/add-bridges.sh
+++ 2-use-case-topologies/host-based-srv6/clab/add-bridges.sh
@@ -2,14 +2,12 @@
 
 sudo brctl addbr xrd06-host
 sudo brctl addbr xrd09-host
-sudo brctl addbr xrd31-host
-sudo brctl addbr xrd32-host
+sudo brctl addbr xrd35-host
 sudo brctl addbr xrd44-host
 sudo brctl addbr xrd45-host
 
 sudo ip link set up xrd06-host
 sudo ip link set up xrd09-host
-sudo ip link set up xrd31-host
-sudo ip link set up xrd32-host
+sudo ip link set up xrd35-host
 sudo ip link set up xrd44-host
 sudo ip link set up xrd45-host
