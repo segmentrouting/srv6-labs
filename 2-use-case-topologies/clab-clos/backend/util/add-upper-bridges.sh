3ea6ea60bfb9e5e987fa9d4af0b398298fc2aba1
brmcdoug <brmcdoug@cisco.com>
2024-11-02 11:29:27 -0700

    lf

--- 2-use-case-topologies/clab-clos/backend/util/add-upper-bridges.sh
+++ 2-use-case-topologies/clab-clos/backend/util/add-upper-bridges.sh
@@ -9,6 +9,15 @@ sudo brctl addbr SS06-host
 sudo brctl addbr SS07-host
 sudo brctl addbr SS08-host
 
+sudo brctl addbr LF65-host1
+sudo brctl addbr LF66-host1
+sudo brctl addbr LF67-host1
+sudo brctl addbr LF68-host1
+sudo brctl addbr LF65-host2
+sudo brctl addbr LF66-host2
+sudo brctl addbr LF67-host2
+sudo brctl addbr LF68-host2
+
 sudo ip link set up SS01-host
 sudo ip link set up SS02-host
 sudo ip link set up SS03-host
@@ -17,3 +26,13 @@ sudo ip link set up SS05-host
 sudo ip link set up SS06-host
 sudo ip link set up SS07-host
 sudo ip link set up SS08-host
+
+sudo ip link set up LF65-host1
+sudo ip link set up LF66-host1
+sudo ip link set up LF67-host1
+sudo ip link set up LF68-host1
+
+sudo ip link set up LF65-host2
+sudo ip link set up LF66-host2
+sudo ip link set up LF67-host2
+sudo ip link set up LF68-host2
