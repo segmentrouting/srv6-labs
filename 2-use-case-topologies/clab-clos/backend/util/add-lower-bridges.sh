0c9a0a853baccfb90f19cdea13f9d48a9287c446
brmcdoug <brmcdoug@cisco.com>
2024-11-03 12:41:53 -0800

    redo topo

--- 2-use-case-topologies/clab-clos/backend/util/add-lower-bridges.sh
+++ 2-use-case-topologies/clab-clos/backend/util/add-lower-bridges.sh
@@ -1,56 +1,85 @@
 #!/bin/sh
 
-sudo brctl addbr SP01-host
-sudo brctl addbr SP02-host
-sudo brctl addbr SP03-host
-sudo brctl addbr SP04-host
-sudo brctl addbr SP05-host
-sudo brctl addbr SP06-host
-sudo brctl addbr SP07-host
-sudo brctl addbr SP08-host
+sudo brctl addbr SP65-host
+sudo brctl addbr SP66-host
+sudo brctl addbr SP67-host
+sudo brctl addbr SP68-host
 
-sudo brctl addbr LF01-host1
-sudo brctl addbr LF02-host1
-sudo brctl addbr LF03-host1
-sudo brctl addbr LF04-host1
-sudo brctl addbr LF05-host1
-sudo brctl addbr LF06-host1
-sudo brctl addbr LF07-host1
-sudo brctl addbr LF08-host1
+sudo brctl addbr LF65-host1
+sudo brctl addbr LF66-host1
+sudo brctl addbr LF67-host1
+sudo brctl addbr LF68-host1
+sudo brctl addbr LF65-host2
+sudo brctl addbr LF66-host2
+sudo brctl addbr LF67-host2
+sudo brctl addbr LF68-host2
 
-sudo brctl addbr LF01-host2
-sudo brctl addbr LF02-host2
-sudo brctl addbr LF03-host2
-sudo brctl addbr LF04-host2
-sudo brctl addbr LF05-host2
-sudo brctl addbr LF06-host2
-sudo brctl addbr LF07-host2
-sudo brctl addbr LF08-host2
+sudo ip link set up SP65-host
+sudo ip link set up SP66-host
+sudo ip link set up SP67-host
+sudo ip link set up SP68-host
 
-sudo ip link set up SP01-host
-sudo ip link set up SP02-host
-sudo ip link set up SP03-host
-sudo ip link set up SP04-host
+sudo ip link set up LF65-host1
+sudo ip link set up LF66-host1
+sudo ip link set up LF67-host1
+sudo ip link set up LF68-host1
 
-sudo ip link set up SP05-host
-sudo ip link set up SP06-host
-sudo ip link set up SP07-host
-sudo ip link set up SP08-host
+sudo ip link set up LF65-host2
+sudo ip link set up LF66-host2
+sudo ip link set up LF67-host2
+sudo ip link set up LF68-host2
 
-sudo ip link set up LF01-host1
-sudo ip link set up LF02-host1
-sudo ip link set up LF03-host1
-sudo ip link set up LF04-host1
-sudo ip link set up LF05-host1
-sudo ip link set up LF06-host1
-sudo ip link set up LF07-host1
-sudo ip link set up LF08-host1
+# sudo brctl addbr SP01-host
+# sudo brctl addbr SP02-host
+# sudo brctl addbr SP03-host
+# sudo brctl addbr SP04-host
+# sudo brctl addbr SP05-host
+# sudo brctl addbr SP06-host
+# sudo brctl addbr SP07-host
+# sudo brctl addbr SP08-host
 
-sudo ip link set up LF01-host2
-sudo ip link set up LF02-host2
-sudo ip link set up LF03-host2
-sudo ip link set up LF04-host2
-sudo ip link set up LF05-host2
-sudo ip link set up LF06-host2
-sudo ip link set up LF07-host2
-sudo ip link set up LF08-host2
+# sudo brctl addbr LF01-host1
+# sudo brctl addbr LF02-host1
+# sudo brctl addbr LF03-host1
+# sudo brctl addbr LF04-host1
+# sudo brctl addbr LF05-host1
+# sudo brctl addbr LF06-host1
+# sudo brctl addbr LF07-host1
+# sudo brctl addbr LF08-host1
+
+# sudo brctl addbr LF01-host2
+# sudo brctl addbr LF02-host2
+# sudo brctl addbr LF03-host2
+# sudo brctl addbr LF04-host2
+# sudo brctl addbr LF05-host2
+# sudo brctl addbr LF06-host2
+# sudo brctl addbr LF07-host2
+# sudo brctl addbr LF08-host2
+
+# sudo ip link set up SP01-host
+# sudo ip link set up SP02-host
+# sudo ip link set up SP03-host
+# sudo ip link set up SP04-host
+
+# sudo ip link set up SP05-host
+# sudo ip link set up SP06-host
+# sudo ip link set up SP07-host
+# sudo ip link set up SP08-host
+
+# sudo ip link set up LF01-host1
+# sudo ip link set up LF02-host1
+# sudo ip link set up LF03-host1
+# sudo ip link set up LF04-host1
+# sudo ip link set up LF05-host1
+# sudo ip link set up LF06-host1
+# sudo ip link set up LF07-host1
+# sudo ip link set up LF08-host1
+
+# sudo ip link set up LF01-host2
+# sudo ip link set up LF02-host2
+# sudo ip link set up LF03-host2
+# sudo ip link set up LF04-host2
+# sudo ip link set up LF05-host2
+# sudo ip link set up LF06-host2
+# sudo ip link set up LF07-host2
+# sudo ip link set up LF08-host2
--- 2-use-case-topologies/clab-clos/backend/util/add-upper-bridges.sh
+++ 2-use-case-topologies/clab-clos/backend/util/add-upper-bridges.sh
@@ -4,35 +4,61 @@ sudo brctl addbr SS01-host
 sudo brctl addbr SS02-host
 sudo brctl addbr SS03-host
 sudo brctl addbr SS04-host
-sudo brctl addbr SS05-host
-sudo brctl addbr SS06-host
-sudo brctl addbr SS07-host
-sudo brctl addbr SS08-host
+# sudo brctl addbr SS05-host
+# sudo brctl addbr SS06-host
+# sudo brctl addbr SS07-host
+# sudo brctl addbr SS08-host
 
-sudo brctl addbr LF65-host1
-sudo brctl addbr LF66-host1
-sudo brctl addbr LF67-host1
-sudo brctl addbr LF68-host1
-sudo brctl addbr LF65-host2
-sudo brctl addbr LF66-host2
-sudo brctl addbr LF67-host2
-sudo brctl addbr LF68-host2
+# sudo brctl addbr LF65-host1
+# sudo brctl addbr LF66-host1
+# sudo brctl addbr LF67-host1
+# sudo brctl addbr LF68-host1
+# sudo brctl addbr LF65-host2
+# sudo brctl addbr LF66-host2
+# sudo brctl addbr LF67-host2
+# sudo brctl addbr LF68-host2
 
 sudo ip link set up SS01-host
 sudo ip link set up SS02-host
 sudo ip link set up SS03-host
 sudo ip link set up SS04-host
-sudo ip link set up SS05-host
-sudo ip link set up SS06-host
-sudo ip link set up SS07-host
-sudo ip link set up SS08-host
+# sudo ip link set up SS05-host
+# sudo ip link set up SS06-host
+# sudo ip link set up SS07-host
+# sudo ip link set up SS08-host
 
-sudo ip link set up LF65-host1
-sudo ip link set up LF66-host1
-sudo ip link set up LF67-host1
-sudo ip link set up LF68-host1
+sudo brctl addbr LF01-host1
+sudo brctl addbr LF02-host1
+sudo brctl addbr LF03-host1
+sudo brctl addbr LF04-host1
+sudo brctl addbr LF05-host1
+sudo brctl addbr LF06-host1
+sudo brctl addbr LF07-host1
+sudo brctl addbr LF08-host1
 
-sudo ip link set up LF65-host2
-sudo ip link set up LF66-host2
-sudo ip link set up LF67-host2
-sudo ip link set up LF68-host2
+sudo brctl addbr LF01-host2
+sudo brctl addbr LF02-host2
+sudo brctl addbr LF03-host2
+sudo brctl addbr LF04-host2
+sudo brctl addbr LF05-host2
+sudo brctl addbr LF06-host2
+sudo brctl addbr LF07-host2
+sudo brctl addbr LF08-host2
+
+sudo ip link set up LF01-host1
+sudo ip link set up LF02-host1
+sudo ip link set up LF03-host1
+sudo ip link set up LF04-host1
+sudo ip link set up LF05-host1
+sudo ip link set up LF06-host1
+sudo ip link set up LF07-host1
+sudo ip link set up LF08-host1
+
+sudo ip link set up LF01-host2
+sudo ip link set up LF02-host2
+sudo ip link set up LF03-host2
+sudo ip link set up LF04-host2
+sudo ip link set up LF05-host2
+sudo ip link set up LF06-host2
+sudo ip link set up LF07-host2
+sudo ip link set up LF08-host2
